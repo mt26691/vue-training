cbf06873713d3245651e499a66d18794b98b990d
mt26691 <tung.nguyen@scopicsoftware.com>
2019-12-03 11:34:24 +0700

    list rendering

--- main.js
+++ main.js
@@ -3,7 +3,17 @@ const app = new Vue({
     data: {
         product: 'first product1',
         description: 'A pair of warm, fuzzy socks',
-        image:'./assets/vmSocks-green-onWhite.jpg',
-        voz:'https://forums.voz.vn/'
+        image: './assets/vmSocks-green-onWhite.jpg',
+        voz: 'https://forums.voz.vn/',
+        inventory: 10,
+        details: [
+            { name: "80% cotton" },
+            { name: "20% polyester" },
+            { name: "Gender neutral" }
+        ],
+        variants: [
+          {"variantId":2234, "variantColor":"green"},
+          {"variantId":2235, "variantColor":"blue"},
+        ],
     }
 });
