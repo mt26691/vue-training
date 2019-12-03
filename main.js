df664a603d3ef18bf31008c4aa6101691242989f
mt26691 <tung.nguyen@scopicsoftware.com>
2019-12-03 14:15:21 +0700

    add @on event

--- main.js
+++ main.js
@@ -12,8 +12,28 @@ const app = new Vue({
             { name: "Gender neutral" }
         ],
         variants: [
-          {"variantId":2234, "variantColor":"green"},
-          {"variantId":2235, "variantColor":"blue"},
+            {
+                "variantId": 2234,
+                "variantColor": "green",
+                "image": "./assets/vmSocks-green-onWhite.jpg",
+                "inStock": true
+            },
+            {
+                "variantId": 2235,
+                "variantColor": "blue",
+                "image": "./assets/vmSocks-blue-onWhite.jpg",
+                "inStock": false
+            },
         ],
+        "inStock": false,
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateProduct(image) {
+            this.image = image;
+        }
     }
 });
