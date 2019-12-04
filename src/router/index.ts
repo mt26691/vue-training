745af7d14fb6a76b14cd302da5b9f3c6d11ed352
mt26691 <tung.nguyen@scopicsoftware.com>
2019-12-04 14:13:57 +0700

    create very first component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import EventList from '../views/EventList.vue'
+import EventCreate from '../views/EventCreate.vue'
+import EventShow from '../views/EventShow.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'event-list',
+    component: EventList
   },
   {
-    path: '/about-us',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    alias: '/about'
+    path: '/event',
+    name: 'event-show',
+    component: EventShow
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate
   }
+  // {
+  //   path: '/about-us',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  //   alias: '/about'
+  // }
 ]
 
 const router = new VueRouter({
