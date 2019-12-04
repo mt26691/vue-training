7582128049a03c25e54c03c8cc92dd5c0ea9b0ce
mt26691 <tung.nguyen@scopicsoftware.com>
2019-12-04 14:05:54 +0700

    router config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    'plugin:prettier/recommended',
+    // 'plugin:prettier/recommended',
     '@vue/standard',
     '@vue/typescript'
   ],
