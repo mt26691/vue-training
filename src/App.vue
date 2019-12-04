90f55eafd6c45254570bf691548b11efe19bcad6
mt26691 <tung.nguyen@scopicsoftware.com>
2019-12-04 14:25:09 +0700

    input router params

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
       <router-link :to="{ name: 'event-list' }">List</router-link> |
       <router-link :to="{ name: 'event-show' }">Show Event #1</router-link>
       <router-link :to="{ name: 'event-create' }">Create</router-link>
+      <router-link :to="{ name: 'user', params: { username: 'mt26691' } }"
+        >MT26691</router-link
+      >
     </div>
     <router-view />
   </div>
---
+++ src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="user">
+    <h1>This is {{ username }}'s page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'User',
+  props: {
+    username: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
+
+<style></style>
