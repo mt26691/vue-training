531bcddd8edc900720c0557737807e8f5eb23ca4
mt26691 <tung.nguyen@scopicsoftware.com>
2019-12-10 11:13:03 +0700

    add getter

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,9 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: { id: 'mt26691', name: 'Manh Tung' }
+    user: { id: 'mt26691', name: 'Manh Tung' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
   },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <h1>Create an event, {{ username }}</h1>
-    <p>This event is created by {{username}}</p>
+    <h1>Create an event, {{ user.name }}</h1>
+    <p>This event is created by {{ user.name }}</p>
+    <p>There are {{catLength}} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
 import Vue from 'vue'
+import { mapState } from 'vuex'
 
 export default Vue.extend({
   name: 'EventCreate',
@@ -18,8 +23,9 @@ export default Vue.extend({
     }
   },
   computed: {
-    username() {
-      return this.$store.state.user.name
+    ...mapState(['user', 'categories']),
+    catLength() {
+      return this.$store.getters.catLength
     }
   },
   created() {}
