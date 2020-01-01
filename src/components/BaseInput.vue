9298c7533c0e003cd335cc3f0117a1eed2d7b5ab
mt26691 <tung.nguyen@scopicsoftware.com>
2020-01-01 15:24:11 +0700

    base input

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input type="text" :value="value" @input="updateValue" v-bind="$attrs" />
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import Vue from 'vue'
+import { mapState, mapGetters } from 'vuex'
+// @ts-ignore
+import DatePicker from 'vuejs-datepicker'
+
+export default Vue.extend({
+  name: 'BaseInput',
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: [String, Number]
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+})
+</script>
+
+<style scoped></style>
